dcf5f9f0540bfa8a622e4f3ceba11b0fed2edf62
ZheWang7 <jameswangzhe1110@gmail.com>
2024-10-21 14:50:49 -0400

    updated LoginUserDataAccessInterface to support tracking the current logged user

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * This method keeps track of which user is logged in.
+     * @param name the nameof the user
+     */
+    void setCurrentUser(String name);
 }
